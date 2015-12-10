a6b33bd25bc7880c2e49ad562e5f4f6d21548dce
Stephen <lonelytreesl@yahoo.com>
2015-12-09 18:12:30 -0800

    Add game introduction

--- wumpus-hunt.py
+++ wumpus-hunt.py
@@ -5,3 +5,8 @@ wumpus_location = choice(cave_numbers)
 player_location =  choice(cave_numbers)
 while wumpus_location == player_location:
     player_location = choice(cave_numbers)
+
+print("Welcome to Wumpus Hunt")
+print("You can see %d caves" % len(cave_numbers))
+print("To play, just type a number")
+print("of the cave you wish to enter next")
