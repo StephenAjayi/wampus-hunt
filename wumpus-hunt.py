4f5bdf6f6cbccff77fc70d4e0f2b5cdba4a2398a
Stephen <lonelytreesl@yahoo.com>
2015-12-14 10:31:12 -0800

    Add warning if Wumpus is nearby.

--- wumpus-hunt.py
+++ wumpus-hunt.py
@@ -10,3 +10,9 @@ print("Welcome to Wumpus Hunt")
 print("You can see %d caves" % len(cave_numbers))
 print("To play, just type a number")
 print("of the cave you wish to enter next")
+
+while True:
+    print("You are in cave", player_location)
+    if (player_location == wumpus_location - 1 or player_location == wumpus_location + 1):
+        print("I smell a Wumpus!")
+    
