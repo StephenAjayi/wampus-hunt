c578c1af3944ff6398c247b5bc02f0acbd6a031a
Stephen <lonelytreesl@yahoo.com>
2015-12-14 10:49:57 -0800

    Add player movement and game ending.

--- wumpus-hunt.py
+++ wumpus-hunt.py
@@ -19,4 +19,8 @@ while True:
     player_input =  raw_input(">")
     if(not player_input.isdigit() or int(player_input) not in cave_numbers):
         print(player_input, "is not a cave!")
-    
+    else:
+        player_location = int(player_input)
+        if player_location == wumpus_location:
+            print "Aargh! You got eatten by a wumpus!"
+            break
